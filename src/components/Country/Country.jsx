d6be8621ed91ffc4392817ea5fcccc3cce56eb40
rsdrahi <rsdrahi13@gmail.com>
2026-03-25 23:10:07 +0600

    css file created on country and countries component. gird added on countries in 3, 1fr. some conditional rendaring and button added which country vidsited.

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import './Country.css'
 
 const Country = ({ country }) => {
   console.log(country.area.area);
+
+  const handleVisited = () => {
+    console.log('btn clicked');
+  }
   return (
-    <div>
+    <div className='country'>
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
       <h3>Name: {country.name.common}</h3>
       <p>Population: {country.population.population}</p>
       <p>Capital: {country.capital.capital}</p>
       <p>Region: {country.region.region}</p>
-      <p>Area: {country.area.area}</p>
+      <p>Area: {country.area.area} {
+        country.area.area > 200000 ?
+          "Big Country" :
+          "Small Country"}</p>
+      <button onClick={handleVisited}>Not Visited</button>
     </div>
   );
 };
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = ({ countriesPromise }) => {
   const countriesData = use(countriesPromise);
@@ -7,9 +8,11 @@ const Countries = ({ countriesPromise }) => {
   return (
     <div>
       <h1>In the Countries: {countries.length}</h1>
-      {
+      <div className='countries'>
+        {
         countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
-      }
+        }
+      </div>
     </div>
   );
 };
