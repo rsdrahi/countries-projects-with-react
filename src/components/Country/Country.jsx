69ebb85f19b38b7fcd5b6b5d7deba65b7d3ecba8
rsdrahi <rsdrahi13@gmail.com>
2026-03-25 22:46:03 +0600

    Country component created. all countries loop and shown on browser.include country flag,name,population,capital,region,area

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const Country = () => {
+const Country = ({ country }) => {
+  console.log(country.area.area);
   return (
     <div>
-        <h3>name: </h3>
+      <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+      <h3>Name: {country.name.common}</h3>
+      <p>Population: {country.population.population}</p>
+      <p>Capital: {country.capital.capital}</p>
+      <p>Region: {country.region.region}</p>
+      <p>Area: {country.area.area}</p>
     </div>
   );
 };
